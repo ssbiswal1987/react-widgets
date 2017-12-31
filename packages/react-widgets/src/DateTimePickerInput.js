9b0ad2abf12ba1e0c6793945fac83742dac87829
swapnalopa <ssbiswal1987@gmail.com>
2017-12-31 13:23:34 +0100

    accept reference and classname

--- packages/react-widgets/src/DateTimePickerInput.js
+++ packages/react-widgets/src/DateTimePickerInput.js
@@ -9,101 +9,102 @@ import * as Props from './util/Props';
 
 class DateTimePickerInput extends React.Component {
 
-  static propTypes = {
-    format: CustomPropTypes.dateFormat.isRequired,
-    editing: PropTypes.bool,
-    editFormat: CustomPropTypes.dateFormat,
-    parse: PropTypes.func.isRequired,
+	static propTypes = {
+		format: CustomPropTypes.dateFormat.isRequired,
+		editing: PropTypes.bool,
+		editFormat: CustomPropTypes.dateFormat,
+		parse: PropTypes.func.isRequired,
 
-    value: PropTypes.instanceOf(Date),
-    onChange: PropTypes.func.isRequired,
-    onBlur: PropTypes.func,
-    culture: PropTypes.string,
+		value: PropTypes.instanceOf(Date),
+		onChange: PropTypes.func.isRequired,
+		onBlur: PropTypes.func,
+		culture: PropTypes.string,
 
-    disabled: CustomPropTypes.disabled,
-    readOnly: CustomPropTypes.disabled,
-  };
+		disabled: CustomPropTypes.disabled,
+		readOnly: CustomPropTypes.disabled,
+	};
 
-  constructor(...args) {
-    super(...args)
+	constructor(...args) {
+		super(...args)
 
-    let { value, editing, editFormat, format, culture } = this.props;
+		let { value, editing, editFormat, format, culture } = this.props;
 
-    this.state = {
-      textValue: formatDate(
-          value
-        , editing && editFormat ? editFormat : format
-        , culture
-      )
-    }
-  }
+		this.state = {
+			textValue: formatDate(
+				value
+				, editing && editFormat ? editFormat : format
+				, culture
+			)
+		}
+	}
 
-  componentWillReceiveProps(nextProps) {
-    let { value, editing, editFormat, format, culture } = nextProps;
+	componentWillReceiveProps(nextProps) {
+		let { value, editing, editFormat, format, culture } = nextProps;
 
-    this.setState({
-      textValue: formatDate(
-          value
-        , editing && editFormat ? editFormat : format
-        , culture
-      )
-    })
-  }
+		this.setState({
+			textValue: formatDate(
+				value
+				, editing && editFormat ? editFormat : format
+				, culture
+			)
+		})
+	}
 
-  handleChange = ({ target: { value } }) => {
-    this._needsFlush = true
-    this.setState({ textValue: value });
-  };
+	handleChange = ({ target: { value } }) => {
+		this._needsFlush = true
+		this.setState({ textValue: value });
+	};
 
-  handleBlur = (event) => {
-    let { format, culture, parse, onChange, onBlur } = this.props;
+	handleBlur = (event) => {
+		let { format, culture, parse, onChange, onBlur } = this.props;
 
-    onBlur && onBlur(event)
+		onBlur && onBlur(event)
 
-    if (this._needsFlush) {
-      let date = parse(event.target.value);
+		if (this._needsFlush) {
+			let date = parse(event.target.value);
 
-      this._needsFlush = false
-      onChange(date, formatDate(date, format, culture))
-    }
-  };
+			this._needsFlush = false
+			onChange(date, formatDate(date, format, culture))
+		}
+	};
 
-  render() {
-    let { disabled, readOnly } = this.props
-    let { textValue } = this.state
+	render() {
+		let { disabled, readOnly, className, reference } = this.props
+		let { textValue } = this.state
 
-    let props = Props.omitOwn(this);
+		let props = Props.omitOwn(this);
 
-    return (
-      <Input
-        {...props}
-        type='text'
-        className="rw-widget-input"
-        value={textValue}
-        disabled={disabled}
-        readOnly={readOnly}
-        onChange={this.handleChange}
-        onBlur={this.handleBlur}
-      />
-    )
-  }
+		return (
+			<Input
+				{...props}
+				type='text'
+				className={`rw-widget-input ${className}`}
+				value={textValue}
+				disabled={disabled}
+				readOnly={readOnly}
+				onChange={this.handleChange}
+				onBlur={this.handleBlur}
+				reference={reference}
+			/>
+		)
+	}
 
-  focus(){
-    findDOMNode(this).focus()
-  }
+	focus() {
+		findDOMNode(this).focus()
+	}
 }
 
 export default DateTimePickerInput;
 
 function isValid(d) {
-  return !isNaN(d.getTime());
+	return !isNaN(d.getTime());
 }
 
-function formatDate(date, format, culture){
-  var val = ''
+function formatDate(date, format, culture) {
+	var val = ''
 
-  if ( (date instanceof Date) && isValid(date) )
-    val = dateLocalizer.format(date, format, culture)
+	if ((date instanceof Date) && isValid(date))
+		val = dateLocalizer.format(date, format, culture)
 
-  return val;
+	return val;
 }
--- packages/react-widgets/src/Input.js
+++ packages/react-widgets/src/Input.js
@@ -3,45 +3,47 @@ import PropTypes from 'prop-types';
 import cn from 'classnames';
 
 class Input extends React.Component {
-  static propTypes = {
-    disabled: PropTypes.bool,
-    readOnly: PropTypes.bool,
-    value: PropTypes.string,
-    type: PropTypes.string,
-    tabIndex: PropTypes.string,
-    component: PropTypes.any,
-  };
+	static propTypes = {
+		disabled: PropTypes.bool,
+		readOnly: PropTypes.bool,
+		value: PropTypes.string,
+		type: PropTypes.string,
+		tabIndex: PropTypes.string,
+		component: PropTypes.any,
+	};
 
-  render() {
-    let {
+	render() {
+		let {
         className
-      , disabled
-      , readOnly
-      , value
-      , tabIndex
-      , type = 'text'
-      , component: Component = 'input'
-      , ...props
+			, disabled
+			, readOnly
+			, value
+			, tabIndex
+			, type = 'text'
+			, component: Component = 'input'
+			, reference
+			, ...props
     } = this.props;
 
-    return (
-      <Component
-        {...props}
-        type={type}
-        tabIndex={tabIndex || 0}
-        autoComplete='off'
-        disabled={disabled}
-        readOnly={readOnly}
-        aria-disabled={disabled}
-        aria-readonly={readOnly}
-        value={value == null ? '' : value}
-        className={cn(
-          className,
-          'rw-input'
-        )}
-      />
-    )
-  }
+		return (
+			<Component
+				{...props}
+				type={type}
+				tabIndex={tabIndex || 0}
+				autoComplete='off'
+				disabled={disabled}
+				readOnly={readOnly}
+				aria-disabled={disabled}
+				aria-readonly={readOnly}
+				value={value == null ? '' : value}
+				className={cn(
+					className,
+					'rw-input'
+				)}
+				ref={input => reference(input)}
+			/>
+		)
+	}
 }
 
 export default Input;
